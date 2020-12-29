4ab91dc90665a49110b3b6b3bf737f8f5e1dd89c
Sanket Thakur <sanketkumar1996@gmail.com>
2020-12-29 17:31:32 +0530

    checking pred

--- check_pred.py
+++ check_pred.py
@@ -1,4 +1,7 @@
-import os
+import os, cv2
+import torch, argparse
+from pathlib import Path
+import numpy as np
 from pipeline_new import FusionPipeline
 from prepare_dataset import IMU_GAZE_FRAME_DATASET, UNIFIED_DATASET
 
@@ -30,9 +33,11 @@ if __name__ == "__main__":
     start_index, end_index = 0, 0
 
     pipeline.eval()
-
+    sliced_imu_dataset, sliced_gaze_dataset, sliced_frame_dataset = None, None, None
+    catList = None
     for index, subDir in enumerate(sorted(os.listdir(pipeline.var.root))):
         pipeline.init_stage()
+
         if 'imu_' in subDir:
             subDir  = subDir + '/' if subDir[-1]!='/' else  subDir
             os.chdir(pipeline.var.root + subDir)
@@ -49,7 +54,6 @@ if __name__ == "__main__":
                 sliced_imu_dataset = uni_imu_dataset[start_index: end_index].detach().cpu().numpy()
                 sliced_gaze_dataset = uni_gaze_dataset[start_index: end_index].detach().cpu().numpy()
 
-                # sliced_frame_dataset = torch.load('framesExtracted_data_' + str(trim_frame_size) + '.pt')
                 sliced_frame_dataset = np.load('framesExtracted_data_' + str(trim_frame_size) + '.npy', mmap_mode='r')
                 unified_dataset = UNIFIED_DATASET(sliced_frame_dataset, sliced_imu_dataset, sliced_gaze_dataset, device)
                 unified_dataloader = torch.utils.data.DataLoader(unified_dataset, batch_size=pipeline.var.batch_size, num_workers=0, drop_last=True)
@@ -58,7 +62,13 @@ if __name__ == "__main__":
                     gaze_data = torch.sum(gaze_data, axis=1) / 8.0
                     coordinates = pipeline(frame_data, imu_data).to(device)
 
+                    if batch_index == 0:
+                        catList = coordinate
+                    else:
+                        catList = torch.cat((catList, coordinate), axis=0)
+
         start_index = end_index + 1
+
 
     video_file = 'scenevideo.mp4'
     capture = cv2.VideoCapture(video_file)
@@ -67,16 +77,32 @@ if __name__ == "__main__":
     print(frame_count, fps)
     capture.set(cv2.CAP_PROP_POS_FRAMES,trim_frame_size)
     ret, frame = capture.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    print(frame.shape)
-
-    # df_gaze = df_gaze.T
-    for i in range(frame_count):
-        if ret == True:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('image', 512, 512)
-            cv2.imshow('image', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            ret, frame = capture.read()
+    # print(frame_count, frame.shape)
+    print(len(sliced_gaze_dataset), sliced_gaze_dataset.shape)
+    print(catList.shape)
+    torch.save(catList, 'pred_test_folder.pt')
+    # coordinate = catList.detach().cpu().numpy()
+    # # df_gaze = df_gaze.T
+    # for i in range(frame_count):
+    #     if ret == True:
+    #         cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    #         # cv2.resizeWindow('image', 512, 512)
+    #         # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #         # coordinate = sliced_gaze_dataset[i]
+    #         gt_gaze_pts = np.sum(sliced_gaze_dataset[i], axis=0) / 4.0
+    #         pred_gaze_pts = coordinate[i]
+    #         print(gaze_pts[0], gaze_pts[1])
+    #         # for index, pt in enumerate(coordinate):
+    #         #     try:
+    #         #         (x, y) = pt[0], pt[1]
+    #         #         frame = cv2.circle(frame, (int(x*frame.shape[1]),int(y*frame.shape[0])), radius=5, color=(0, 0, 255), thickness=5)
+    #         #     except Exception as e:
+    #         #         print(e)
+    #         frame = cv2.circle(frame, (int(gt_gaze_pts[0]*frame.shape[1]),int(gt_gaze_pts[1]*frame.shape[0])), radius=5, color=(0, 0, 255), thickness=5)
+    #         frame = cv2.circle(frame, (int(pred_gaze_pts[0]*frame.shape[1]),int(pred_gaze_pts[1]*frame.shape[0])), radius=5, color=(0, 255, 0), thickness=5)
+    #         cv2.imshow('image', frame)
+    #
+    #         if cv2.waitKey(1) & 0xFF == ord('q'):
+    #             break
+    #         # cv2.waitKey(0)
+    #         ret, frame = capture.read()
